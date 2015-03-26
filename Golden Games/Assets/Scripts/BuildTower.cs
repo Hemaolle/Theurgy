b837c8d3b8502d0eaaa352852b16a52ed16a1bd2
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-03-26 19:17:43 +0200

    Began work on tower placement script

---
+++ Golden Games/Assets/Scripts/BuildTower.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildTower : MonoBehaviour {
+
+	public GameObject tower;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	private RaycastHit hit;   
+
+	void Update () {
+		int groundLayermask = 1 << LayerMask.NameToLayer ("Ground");
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast (ray, out hit, Mathf.Infinity, groundLayermask)) { //&& Input.GetMouseButtonDown (0) && hit.transform.name == "Play"
+			tower.transform.position = hit.point;
+		}
+		
+	}
+}
