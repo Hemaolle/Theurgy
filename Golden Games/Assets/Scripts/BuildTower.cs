fab27e8380fadd614e590f80b42288cc28e373d3
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-03-26 20:05:13 +0200

    Towers can be placed
    -Also smooth shading for the enemy.

--- Golden Games/Assets/Scripts/BuildTower.cs
+++ Golden Games/Assets/Scripts/BuildTower.cs
@@ -5,12 +5,14 @@ public class BuildTower : MonoBehaviour {
 
 	public GameObject tower;
 
+
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 
-	private RaycastHit hit;   
+	private RaycastHit hit;  
 
 	void Update () {
 		int groundLayermask = 1 << LayerMask.NameToLayer ("Ground");
@@ -19,6 +21,11 @@ public class BuildTower : MonoBehaviour {
 		if (Physics.Raycast (ray, out hit, Mathf.Infinity, groundLayermask)) { //&& Input.GetMouseButtonDown (0) && hit.transform.name == "Play"
 			tower.transform.position = hit.point;
 		}
-		
+
+		if (Input.GetMouseButtonDown (0)) {
+			GameObject newTower = Instantiate(tower, hit.point, Quaternion.identity) as GameObject;
+			newTower.GetComponent<BuildTower>().tower = newTower;
+			Destroy(this);
+		}
 	}
 }
