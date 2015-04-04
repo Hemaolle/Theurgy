3e9d6d7eebe77601a481c5125abde033211abd1f
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-04 21:52:14 +0300

    The towers shoot the closest enemy and have max range

--- Golden Games/Assets/Scripts/TowerShooting.cs
+++ Golden Games/Assets/Scripts/TowerShooting.cs
@@ -5,8 +5,8 @@ public class TowerShooting : MonoBehaviour {
 
 	public GameObject bullet;
 	public GameObject shootingPosition;
-
 	public float shootInterval;
+	public float maxRange = 4;
 
 	// Use this for initialization
 	void Start () {
@@ -19,7 +19,23 @@ public class TowerShooting : MonoBehaviour {
 	}
 
 	void Shoot() {
-		GameObject newBullet = Instantiate (bullet, shootingPosition.transform.position, Quaternion.identity) as GameObject;
-		newBullet.GetComponent<Projectile> ().target = GameObject.FindWithTag ("Enemy");
+		// TODO: should maybe start shooting immediately when enemy gets close enough, now only checks between shootInterval times
+		if (Vector3.Distance (transform.position, FindClosestWithTag ("Enemy").transform.position) < maxRange) {
+			GameObject newBullet = Instantiate (bullet, shootingPosition.transform.position, Quaternion.identity) as GameObject;
+			newBullet.GetComponent<Projectile> ().target = FindClosestWithTag ("Enemy");
+		}
+	}
+
+	GameObject FindClosestWithTag(string tag) {
+		GameObject closestSoFar = null;
+		float closestDistance = float.MaxValue;
+		foreach (GameObject candidate in GameObject.FindGameObjectsWithTag(tag)) {
+			float candidateDistance = Vector3.Distance(transform.position, candidate.transform.position);
+			if (candidateDistance < closestDistance) {
+				closestSoFar = candidate;
+				closestDistance = candidateDistance;
+			}
+		}
+		return closestSoFar;
 	}
 }
