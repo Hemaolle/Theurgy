fa7b558aeb60c0c847b63dc0c06cd30ea434e8de
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-04 22:56:20 +0300

    The enemies have healthbars that follow them correctly but are drawn ortographically

---
+++ Golden Games/Assets/Scripts/OrtographicCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrtographicCamera : MonoBehaviour {
+
+	static GameObject instance;
+	
+	void Awake() {
+		instance = gameObject;
+	}
+	
+	public static GameObject Get() {
+		return instance;
+	}
+}
