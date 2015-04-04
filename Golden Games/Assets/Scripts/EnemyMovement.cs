6eaef61dbb504dd973a7df13b6d3d10804294e83
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-04 21:28:51 +0300

    The enemy rotates over time in corners instead of rotating in a single frame

--- Golden Games/Assets/Scripts/EnemyMovement.cs
+++ Golden Games/Assets/Scripts/EnemyMovement.cs
@@ -4,47 +4,36 @@ using System.Collections;
 public class EnemyMovement : MonoBehaviour {
 
 	public float speed = 1;
+	public float rotationSpeed = 1;
 	public Transform[] path;
-	public float startingDelay = 1;
-	Vector3 direction;
 	int pointNumber;
-	bool moving = false;
-	float sensitivity = 0.05f;
 	
 	void Start () {
-		Invoke ("StartMoving", startingDelay);
 		transform.position = path [0].position;
+		transform.rotation = Quaternion.LookRotation (path [pointNumber + 1].position - path [pointNumber].position, Vector3.up);
+		MoveToNextPoint ();
 	}
-	
-	void StartMoving () {
-		moving = true;
+
+	void RotateToNextDirection() {
+		Quaternion newRotation = Quaternion.LookRotation (path [pointNumber + 1].position - path [pointNumber].position, Vector3.up);
+		Vector3 newRotationEuler = newRotation.eulerAngles;
+		iTween.RotateTo(gameObject, iTween.Hash("x", newRotationEuler.x,
+		                "y", newRotationEuler.y,
+		                "z", newRotationEuler.z,
+		                "speed", rotationSpeed,
+		                "easetype", iTween.EaseType.linear,
+		                "oncomplete", "MoveToNextPoint" ));
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		// kun tarpeeksi aikaa kulunut, kameran liike alkaa
-		//		if (Time.timeSinceLevelLoad >= startingDelay) {
-		//			moving = true;
-		//		}
-		
-		if (moving == true) {
-			// kun saavutetaan viimeinen piste, pysäytetään kamera
-			if (Vector3.Distance(transform.position, path[path.Length - 1].position) <= sensitivity) {
-				Destroy(this);
-				return;
-			}
-			
-			//etsitään vektorien välinen suunta kun ollaan tarpeeksi lähellä seuraavaa käännöspistettä
-			if (Vector3.Distance(transform.position, path[pointNumber].position) <= sensitivity) {
-				direction = path[pointNumber+1].position - path[pointNumber].position;
-				direction.Normalize();
-				pointNumber++;
-				transform.LookAt(path[pointNumber], Vector3.up);
-			}
-			
-			//liikutaan haluttuun suuntaan
-			transform.position += direction * Time.deltaTime * speed;
+
+	void MoveToNextPoint() {
+		pointNumber++;
+		if (pointNumber < path.Length) {
+			iTween.MoveTo (gameObject, iTween.Hash ("x", path [pointNumber].position.x,
+		                                      "y", path [pointNumber].position.y,
+		                                      "z", path [pointNumber].position.z,
+		                                      "speed", speed,
+		                                      "easetype", iTween.EaseType.linear,
+		                                      "oncomplete", "RotateToNextDirection"));
 		}
 	}
 }
