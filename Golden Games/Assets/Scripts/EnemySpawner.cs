cd519abf1b2207b3a14107f3306f4630ee88e756
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-04 21:44:11 +0300

    Multiple enemies are spawned

---
+++ Golden Games/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+	public GameObject enemy;
+	public float interval;
+	public Transform[] path;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnEnemy", 0, interval);
+	}
+
+	void SpawnEnemy() {
+		EnemyMovement enemyMovement = (Instantiate (enemy, transform.position, Quaternion.identity) 
+		                               as GameObject).GetComponent<EnemyMovement> ();
+		enemyMovement.path = path;
+	}
+}
