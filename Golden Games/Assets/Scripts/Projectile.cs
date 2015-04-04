37420c325ea43df95945b6d5b3d2e46afd85a05d
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-04 23:14:05 +0300

    Bullets don't stop in the air if their target is destroyed.

--- Golden Games/Assets/Scripts/Projectile.cs
+++ Golden Games/Assets/Scripts/Projectile.cs
@@ -7,16 +7,35 @@ public class Projectile : MonoBehaviour {
 	public float damage = 1;
 	public float speed = 5;
 
+	Vector3 targetLocation;
+	bool targetAlive = true;
+
 	// Use this for initialization
 	void Start () {
-	
+		targetLocation = target.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 direction = target.transform.position - transform.position;
-		direction.Normalize ();
-		transform.position += direction * Time.deltaTime * speed;
+		if (target == null) {
+			targetAlive = false;
+			iTween.MoveTo(gameObject, iTween.Hash("x", targetLocation.x,
+			                                      "y", targetLocation.y,
+			                                      "z", targetLocation.z,
+			                                      "speed", speed,
+			                                      "easetype", iTween.EaseType.linear,
+			                                      "oncomplete", "DestroyThis"));
+		}
+		if (targetAlive) {
+			Vector3 direction = target.transform.position - transform.position;
+			direction.Normalize ();
+			transform.position += direction * Time.deltaTime * speed;
+		}
+
+	}
+
+	void DestroyThis() {
+		Destroy (gameObject);
 	}
 
 	void OnTriggerEnter(Collider collider) {
