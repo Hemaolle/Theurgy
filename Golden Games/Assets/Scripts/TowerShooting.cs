1cbb4f3fa3cdbfeabdd9b775db3e76130d681a66
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-05 12:37:49 +0300

    The towers keep shooting the same enemy until it dies or goes out of range

--- Golden Games/Assets/Scripts/TowerShooting.cs
+++ Golden Games/Assets/Scripts/TowerShooting.cs
@@ -8,6 +8,8 @@ public class TowerShooting : MonoBehaviour {
 	public float shootInterval;
 	public float maxRange = 4;
 
+	private GameObject target;
+
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating("Shoot", shootInterval, shootInterval);
@@ -19,10 +21,13 @@ public class TowerShooting : MonoBehaviour {
 	}
 
 	void Shoot() {
+		if (target == null || Vector3.Distance (target.transform.position, transform.position) > maxRange) {
+			target = FindClosestWithTag ("Enemy");
+		}
 		// TODO: should maybe start shooting immediately when enemy gets close enough, now only checks between shootInterval times
 		if (Vector3.Distance (transform.position, FindClosestWithTag ("Enemy").transform.position) < maxRange) {
 			GameObject newBullet = Instantiate (bullet, shootingPosition.transform.position, Quaternion.identity) as GameObject;
-			newBullet.GetComponent<Projectile> ().target = FindClosestWithTag ("Enemy");
+			newBullet.GetComponent<Projectile> ().target = target;
 		}
 	}
 
