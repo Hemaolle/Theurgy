b2285e3136365960333fae2bdc4a950bb432d55f
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-05 12:43:21 +0300

    Some renaming on the script that is now called NoPerspectiveWorldLocation

---
+++ Golden Games/Assets/Scripts/FloatingText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatAndDestroy : MonoBehaviour {
+
+	public float speed;
+	public float distance;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 target = transform.position + Vector3.up * distance;
+		iTween.MoveTo (gameObject, iTween.Hash ("x", target.x,
+		                                        "y", target.y,
+		                                        "z", target.z,
+		                                        "speed", speed,
+		                                        "easetype", iTween.EaseType.linear,
+		                                        "oncomplete", "DestroyGameObject"));
+	}
+
+	void DestroyGameObject() {
+		Destroy (gameObject);
+	}
+}
---
+++ Golden Games/Assets/Scripts/NoPerspectiveWorldLocation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoPerspectiveWorldLocation : MonoBehaviour {
+
+	public GameObject UIElement;
+	public GameObject WorldLocation;
+
+	Quaternion rotation;
+	
+	void Awake()
+	{
+		rotation = UIElement.transform.rotation;
+	}
+	
+	void LateUpdate()
+	{        
+		var position = MainCamera.Get().GetComponent<Camera>().WorldToNormalizedViewportPoint(WorldLocation.transform.position);
+		UIElement.transform.position = OrtographicCamera.Get().GetComponent<Camera>().NormalizedViewportToWorldPoint(position);
+		UIElement.transform.rotation = rotation;
+	}
+
+}
