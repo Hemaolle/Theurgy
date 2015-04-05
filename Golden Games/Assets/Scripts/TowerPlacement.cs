f610da1dc194e1ae898f5d862a79563cfa92109a
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-05 12:13:02 +0300

    Tower is placed at mouse position already at start so that it doesn't flash at 0,0,0 when creating

--- Golden Games/Assets/Scripts/TowerPlacement.cs
+++ Golden Games/Assets/Scripts/TowerPlacement.cs
@@ -5,22 +5,26 @@ public class TowerPlacement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		PlaceAtMousePosition ();
 	}
 
 	private RaycastHit hit;  
 
 	void Update () {
-		int groundLayermask = 1 << LayerMask.NameToLayer ("Ground");
-
-		Ray ray = MainCamera.Get().GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
-		if (Physics.Raycast (ray, out hit, Mathf.Infinity, groundLayermask)) { //&& Input.GetMouseButtonDown (0) && hit.transform.name == "Play"
-			transform.position = hit.point;
-		}
-
+		PlaceAtMousePosition ();
 		if (Input.GetMouseButtonDown (0)) {
 			GetComponent<TowerShooting>().enabled = true;
 			Destroy(this);
+		}
+	}
+
+	void PlaceAtMousePosition ()
+	{
+		int groundLayermask = 1 << LayerMask.NameToLayer ("Ground");
+		Ray ray = MainCamera.Get ().GetComponent<Camera> ().ScreenPointToRay (Input.mousePosition);
+		if (Physics.Raycast (ray, out hit, Mathf.Infinity, groundLayermask)) {
+			//&& Input.GetMouseButtonDown (0) && hit.transform.name == "Play"
+			transform.position = hit.point;
 		}
 	}
 }
