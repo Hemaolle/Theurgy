bf70a786afd34cc7901f41f18af45708e01086b0
Oskari Leppäaho <oskari.leppaaho@gmail.com>
2015-04-09 20:21:34 +0300

    Started work on wave spawning

--- Golden Games/Assets/Scripts/EnemySpawner.cs
+++ Golden Games/Assets/Scripts/EnemySpawner.cs
@@ -7,14 +7,22 @@ public class EnemySpawner : MonoBehaviour {
 	public float interval;
 	public Transform[] path;
 
+	public Wave[] waves;
+
 	// Use this for initialization
 	void Start () {
 		InvokeRepeating ("SpawnEnemy", 0, interval);
 	}
 
-	void SpawnEnemy() {
+	void SpawnEnemy(GameObject enemy) {
 		EnemyMovement enemyMovement = (Instantiate (enemy, transform.position, Quaternion.identity) 
 		                               as GameObject).GetComponent<EnemyMovement> ();
 		enemyMovement.path = path;
+	}
+
+	void RunWave(Wave wave) {
+		for (int i = 0; i < wave.numberOfEnemies; i++) {
+			SpawnEnemy(wave.enemy);
+		}
 	}
 }
