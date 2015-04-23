c32f3079874100f3ed59de438a2a5385302ceed3
garbage <garbage>
2015-04-23 18:25:49 +0300

    Added font to Gold count

--- Golden Games/Assets/Scripts/EnemySpawner.cs
+++ Golden Games/Assets/Scripts/EnemySpawner.cs
@@ -14,7 +14,11 @@ public class EnemySpawner : MonoBehaviour {
 		InvokeRepeating ("SpawnEnemy", 0, interval);
 	}
 
-	void SpawnEnemy(){//GameObject enemy) {
+	void SpawnEnemy() {
+		SpawnEnemy (enemy);
+	}
+
+	void SpawnEnemy(GameObject enemy) {
 		EnemyMovement enemyMovement = (Instantiate (enemy, transform.position, Quaternion.identity) 
 		                               as GameObject).GetComponent<EnemyMovement> ();
 		enemyMovement.path = path;
